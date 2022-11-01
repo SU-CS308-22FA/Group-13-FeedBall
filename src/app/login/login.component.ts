9321e0b149f1936e91d31ce9478c13e6c83d542d
bilgesucakir <bilgesucakir@sabanciuniv.edu>
2022-11-01 16:14:12 +0300

    some fixes login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent{
+
+
+  onLogin(form: NgForm){
+
+    console.log("in onLogin");
+    if(form.invalid){
+      return;
+    }
+
+
+    //maybe password rules: 8 chars, etc. etc.
+
+    form.resetForm();
+  }
 
 }
