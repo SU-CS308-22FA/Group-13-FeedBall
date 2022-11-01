76c00b61e90e09c638634d83a3cbab2d4cae6df7
omertarakci <omertarakci@sabanciuniv.edu>
2022-11-01 17:34:07 +0300

    fIntegration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAPdkf7CmkZb-nSKmd6VtRf1xO0js5BqQo",
+    authDomain: "feedball-a306e.firebaseapp.com",
+    projectId: "feedball-a306e",
+    storageBucket: "feedball-a306e.appspot.com",
+    messagingSenderId: "1089975044270",
+    appId: "1:1089975044270:web:c28a6119edbf9e119d853f",
+    measurementId: "G-CWBGE3VFZP"
+    }
 };
 
 /*
