b2bba272355ee25f374e7dba56060d4778eb58fb
omertarakci <omertarakci@sabanciuniv.edu>
2022-11-01 19:43:19 +0300

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from "./shared/services/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+
+  constructor(
+    public authService: AuthService
+  )
+  {}
   title = 'feedball_project';
 }
