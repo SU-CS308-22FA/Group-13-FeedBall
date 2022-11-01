0b9a2ab1871f4603165aaeabfd70bef9733f0fe0
selinn-ilhan <selinilhan@sabanciuniv.edu>
2022-11-01 18:21:18 +0300

    signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent{
+  onSignUp(form: NgForm){
+    console.log("in onSignUp");
+    if(form.invalid){
+      return;
+    }
+
+
+    //maybe password rules: 8 chars, etc. etc.
+
+    form.resetForm();
+  }
 
 }
