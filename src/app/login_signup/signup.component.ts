38ba98a6d88268ee3648c28c4201df0d5b433ba1
Bilgesu Çakır <95298678+bilgesucakir@users.noreply.github.com>
2022-11-01 13:57:54 +0300

    Add files via upload

---
+++ src/app/login_signup/signup.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['signup.component.css']
+})
+
+export class SignupComponent{
+
+  constructor(){}
+}
