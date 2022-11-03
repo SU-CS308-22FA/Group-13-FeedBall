d9f2f9d404490a73c14ff7ad5c0f7689da8766c4
Bilgesu Çakır <95298678+bilgesucakir@users.noreply.github.com>
2022-11-03 18:05:39 +0300

    some adjustments for updating info

---
+++ src/app/profile/edit_profile.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { AuthService } from "../shared/services/auth.service";
+import { User } from "../shared/services/user";
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { animationFrameScheduler, of, switchMap } from 'rxjs';
+import { Auth, getAdditionalUserInfo, updateCurrentUser } from 'firebase/auth';
+import * as firebase from 'firebase/compat';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './edit_profile.component.html',
+  styleUrls: ['./edit_profile.component.css']
+})
+export class EditProfileComponent{
+
+  constructor(
+    public afAuth: AngularFireAuth,
+    public afs: AngularFirestore,
+    private authService: AuthService,
+    private router: Router){
+
+  }
+
+
+  user$ = this.authService.user$;
+
+
+  callLogOut(){
+    this.authService.SignOut();
+  }
+
+
+  navigateMainPage(){
+    this.router.navigate(['feed']);
+  }
+
+  navigateProfilePage(){
+    this.router.navigate(['profile']);
+  }
+
+  onEditInfo(form: NgForm){
+    if(form.invalid){
+      return;
+    }
+    else{
+
+    }
+  }
+
+
+
+}
