81cbca04c2bcd086dff3a38421365cd3460452f6
Bilgesu Çakır <95298678+bilgesucakir@users.noreply.github.com>
2022-12-08 17:06:52 +0300

    inmatch messages display done

--- src/app/in-match/in-match.component.ts
+++ src/app/in-match/in-match.component.ts
@@ -1,19 +1,20 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, Pipe, PipeTransform } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import { AuthService } from "../shared/services/auth.service";
-
+import {MatButtonModule} from '@angular/material/button';
 import { User } from "../shared/services/user";
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import {
   AngularFirestore,
+  AngularFirestoreCollection,
   AngularFirestoreDocument,
 } from '@angular/fire/compat/firestore';
-import { animationFrameScheduler, of, switchMap } from 'rxjs';
+import { animationFrameScheduler, Observable, of, switchMap } from 'rxjs';
 import { Auth, getAdditionalUserInfo, updateCurrentUser } from 'firebase/auth';
 import * as firebase from 'firebase/compat';
 import { Router } from '@angular/router';
-import { Messages } from '../models/messages.model';
+import { messages } from '../models/messages.model';
 
 @Component({
   selector: 'app-in-match',
@@ -22,39 +23,59 @@ import { Messages } from '../models/messages.model';
 })
 export class InMatchComponent{
 
+
+  messagesRef: AngularFirestoreCollection<messages>;
+  messages$: Observable<messages[]>;
+
+
   constructor(
     public authService: AuthService,
     public afs: AngularFirestore,
-  ){}
+    public auth: AngularFireAuth
+  ){
+    this.messagesRef = this.afs.collection('messages');
+    this.messages$ = this.messagesRef.valueChanges();
+    console.log(this.messagesRef);
+  }
+
+
   user$ = this.authService.user$;
 
+  dummyMatch = "GS:FB/07.12.22/21.30:23.00";
   className = "rightdiv";
 
   dummyelems: number[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-  onSubmitForm(){
+  onSubmitForm(form: NgForm){
+
+    console.log(form.value.content);
+
 
 
     //this.createMessageAndPushToDatabase();
+
+
+    form.resetForm();
   }
 
 
 
-  createMessageAndPushToDatabase(messageContent: string, messageDate: Date, userID: string, currentMatchCode: string){
+  createMessageAndPushToDatabase(messageContent: string, messageDate: Date, user: User, currentMatchCode: string){
 
     let emptyList: Array<String> = [];
     let id = "";
 
-    const sendMessage: Messages = {
+    const sendMessage: messages = {
       mid: "",
       matchCode: currentMatchCode,
-      messageOwnerID: userID,
+      messageOwnerID: user.uid,
+      messageOwnerName: user.name,
+      messageOwnerSurname: user.surname,
       sentWhen: messageDate,
       content: messageContent,
       likeNumber: 0,
-      dislikeNumber: 0,
       likeList: emptyList,
-      dislikeList: emptyList
+
     }
 
 
@@ -86,4 +107,39 @@ export class InMatchComponent{
 
   }
 
+}
+
+
+
+@Pipe({ name: 'hoursminutespipe'})
+export class HoursMinutesPipe implements PipeTransform {
+transform(timestamp: any) {
+
+  var numtimestamp = Number(timestamp.seconds);
+  numtimestamp = numtimestamp * 1000;
+  const dateOf = new Date(numtimestamp);
+  var returnStr = "";
+
+  var mins = dateOf.getMinutes().toString();
+  if(Object.keys(mins).length < 2){
+    mins = "0" + mins;
+  }
+  if(Object.keys(mins).length < 1){
+    mins = "00" + mins;
+  }
+
+  var hours = dateOf.getHours().toString();
+  if(Object.keys(hours).length < 2){
+    hours = "0" + hours;
+  }
+  if(Object.keys(hours).length < 1){
+    hours = "00" + hours;
+  }
+
+
+  returnStr = hours + ":" + mins;
+
+
+  return returnStr;
+  }
 }
